eb16110d67e37afbdabac08c6a321c2e3fa8e9cf
_Ghostrick_ <ghostrick.kinako@gmail.com>
2017-11-12 22:05:48 +0900

    get Joy-Con's event.

---
+++ index.js
@@ -0,0 +1,14 @@
+const HID = require('node-hid')
+const devices = HID.devices()
+
+let JoyCon = []
+devices.some(device => { if((resp = device.product.match(/Joy-Con \(([^\]]*)\)/)) && resp[1]) JoyCon[resp[1]] = device})
+
+// if not found controller, process.exit()
+if(!JoyCon['R']) console.log('Not found Joy-Con (R)') || process.exit()
+
+// Open device(Joy-Con (R))
+let JoyConR = new HID.HID(JoyCon['R'].vendorId, JoyCon['R'].productId)
+
+// Registering event handler
+JoyConR.on('data', data => { console.log(data.toString('hex', 0, data.length)) })
